01e73299e570c62758efc28585c79269e0af1ea8
aditprq <143347564+aditprq@users.noreply.github.com>
2024-08-14 23:10:30 +0530

    latest

--- routes/booking.js
+++ routes/booking.js
@@ -1,29 +1,29 @@
-// const express = require('express');
-// const router = express.Router();
-// const path = require('path');
-
-
-
-// router.get('/electrician', (req,res)=>{
-//     res.render('bookingForms/electrician');
-// });
-
-
-// router.get('/',(req,res)=>{
-//     res.send('some');
-// })
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-// Example booking route (implement as needed)
-router.get('/', (req, res) => {
-  res.send('Booking Home');
+
+
+router.get('/electrician', (req,res)=>{
+    res.render('bookingForms/electrician');
 });
 
-// Add other booking-related routes here
+
+router.get('/',(req,res)=>{
+    res.send('some');
+})
 
 module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+
+// // Example booking route (implement as needed)
+// router.get('/', (req, res) => {
+//   res.send('Booking Home');
+// });
+
+// // Add other booking-related routes here
+
+// module.exports = router;
