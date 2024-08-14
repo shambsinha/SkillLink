4e7a4ab4c81f44e21f1bb83903c16e93a70c33e8
aditprq <143347564+aditprq@users.noreply.github.com>
2024-08-14 23:08:21 +0530

    new push

--- server.js
+++ server.js
@@ -75,6 +75,7 @@
 //   res.render('login', { message: '' });
 // });
 
+<<<<<<< Updated upstream
 // app.get('/signup', (req, res) => {
 //   res.render('signup', { message: '' });
 // });
@@ -206,3 +207,9 @@
 //   if (err) console.log(err);
 //   else console.log('Server running on port 5050');
 // });
+=======
+app.listen(5050, (err) => {
+  if (err) console.log(err);
+  else console.log('Server running on port 3000');
+});
+>>>>>>> Stashed changes
